fe95f234cb4caf1f7da0b3d9a22198c16ef40d87
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2025-09-19 21:21:42 +0200

    added flake

---
+++ flake.nix
@@ -0,0 +1,116 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    rust-overlay,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [rust-overlay.overlays.default];
+      };
+
+      osxDependencies = with pkgs;
+        lib.optionals stdenv.isDarwin
+        [
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.CoreServices
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+
+      cargoTomlContents = builtins.readFile ./Cargo.toml;
+
+      version = (builtins.fromTOML cargoTomlContents).package.version;
+      rustVersion = "1.88.0";
+
+      rustToolchain = pkgs.rust-bin.stable.${rustVersion}.default;
+
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      };
+
+      iaiken = rustPlatform.buildRustPackage {
+        inherit version;
+
+        name = "iaiken";
+
+        buildInputs = with pkgs; [openssl] ++ osxDependencies;
+        nativeBuildInputs = with pkgs; [pkg-config openssl.dev];
+
+        src = pkgs.lib.cleanSourceWith {src = self;};
+        doCheck = false; # don’t run cargo test
+        CARGO_BUILD_TESTS = "false"; # don’t even compile test binaries
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        meta = with pkgs.lib; {
+          description = "Jupyter kernel for Aiken smart contract language";
+          homepage = "https://github.com/rober-m/iaiken";
+          license = licenses.asl20;
+          mainProgram = "iaiken";
+        };
+      };
+
+      aiken-repl = rustPlatform.buildRustPackage {
+        inherit version;
+
+        name = "aiken-repl";
+
+        buildInputs = with pkgs; [openssl] ++ osxDependencies;
+        nativeBuildInputs = with pkgs; [pkg-config openssl.dev];
+
+        src = pkgs.lib.cleanSourceWith {src = self;};
+        doCheck = false; # don’t run cargo test
+        CARGO_BUILD_TESTS = "false"; # don’t even compile test binaries
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        meta = with pkgs.lib; {
+          description = "REPL for the Aiken smart contract language";
+          homepage = "https://github.com/rober-m/iaiken";
+          license = licenses.asl20;
+          mainProgram = "aiken-repl";
+        };
+      };
+
+      packages = {
+        iaiken = iaiken;
+        aiken-repl = aiken-repl;
+        default = packages.iaiken;
+      };
+
+      overlays.default = final: prev: {iaiken = packages.iaiken;};
+
+      gitRev =
+        if (builtins.hasAttr "rev" self)
+        then self.rev
+        else "dirty";
+    in {
+      inherit packages overlays;
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [
+            pkg-config
+            openssl
+            cargo-insta
+            (rustToolchain.override {
+              extensions = ["rust-src" "clippy" "rustfmt" "rust-analyzer"];
+            })
+          ]
+          ++ osxDependencies;
+
+        shellHook = ''
+          export GIT_REVISION=${gitRev}
+        '';
+      };
+    });
+}
