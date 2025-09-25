a1c543a715703962da53720266697720f5173a8f
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2025-09-26 01:00:51 +0200

    protocol to 5.4

--- crates/iaiken/src/messages/shell/kernel_info.rs
+++ crates/iaiken/src/messages/shell/kernel_info.rs
@@ -1,14 +1,14 @@
 use serde::{Deserialize, Serialize};
 
-pub const PROTOCOL_VERSION: &str = "5.3";
+pub const PROTOCOL_VERSION: &str = "5.4";
 const KI_STATUS: &str = "ok"; // TODO: Handle error status
 const KI_IMPLEMENTATION: &str = "aiken";
 const KI_IMPLEMENTATION_VERSION: &str = "0.0.1";
 const KI_BANNER: &str = "Aiken Kernel v0.1.0\nCardano Smart Contract Language";
 const KI_DEBUGGER: bool = false;
 const KI_LI_NAME: &str = "aiken";
-const KI_LI_VERSION: &str = "1.0.0"; //TODO
-const KI_LI_MIMETYPE: &str = "text/x-aiken"; //TODO?
+const KI_LI_VERSION: &str = "0.0.1"; //TODO: Change to actual Aiken version
+const KI_LI_MIMETYPE: &str = "text/x-aiken";
 const KI_LI_FILE_EXT: &str = ".ak";
 
 // DOCS: https://jupyter-client.readthedocs.io/en/latest/messaging.html#kernel-info
@@ -29,6 +29,24 @@ pub struct KernelInfoReply {
     pub supported_features: Option<Vec<String>>, // A list of optional features such as 'debugger' and 'kernel subshells'.
 }
 
+// DOCS: https://jupyter-client.readthedocs.io/en/latest/messaging.html#kernel-info
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct LanguageInfo {
+    pub name: String,     // Name of the programming language that the kernel implements
+    pub version: String,  // Language version number. Format X.Y.Z
+    pub mimetype: String, // mimetype for script files in this language
+    pub file_extension: String, // Extension including the dot, e.g. '.py' or '.ak'
+    pub pygments_lexer: Option<String>, // Pygments lexer, for highlighting. Only needed if it differs from the 'name' field.
+    pub codemirror_mode: Option<String>, // Codemirror mode, for highlighting in the notebook.. Only needed if it differs from the 'name' field.
+    pub nbconvert_exporter: String,      // Nbconvert exporter
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct HelpLink {
+    pub text: String,
+    pub url: String,
+}
+
 impl KernelInfoReply {
     pub fn new() -> Self {
         KernelInfoReply {
@@ -41,8 +59,8 @@ impl KernelInfoReply {
                 version: KI_LI_VERSION.to_string(),
                 mimetype: KI_LI_MIMETYPE.to_string(),
                 file_extension: KI_LI_FILE_EXT.to_string(),
-                pygments_lexer: Some("text".to_string()),
-                codemirror_mode: Some(serde_json::Value::String("aiken".to_string())),
+                pygments_lexer: Some(KI_LI_NAME.to_string()),
+                codemirror_mode: Some(KI_LI_NAME.to_string()),
                 nbconvert_exporter: "script".to_string(),
             },
             banner: KI_BANNER.to_string(),
@@ -51,25 +69,7 @@ impl KernelInfoReply {
                 text: "Aiken Documentation".to_string(),
                 url: "https://aiken-lang.org/".to_string(),
             }],
-            supported_features: None, //TODO
+            supported_features: None,
         }
     }
-}
-
-// DOCS: https://jupyter-client.readthedocs.io/en/latest/messaging.html#kernel-info
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct LanguageInfo {
-    pub name: String,     // Name of the programming language that the kernel implements
-    pub version: String,  // Language version number. Format X.Y.Z
-    pub mimetype: String, // mimetype for script files in this language
-    pub file_extension: String, // Extension including the dot, e.g. '.py' or '.ak'
-    pub pygments_lexer: Option<String>, // Pygments lexer, for highlighting. Only needed if it differs from the 'name' field.
-    pub codemirror_mode: Option<serde_json::Value>, // Codemirror mode, for highlighting in the notebook.. Only needed if it differs from the 'name' field.
-    pub nbconvert_exporter: String,                 // Nbconvert exporter
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct HelpLink {
-    pub text: String,
-    pub url: String,
 }
